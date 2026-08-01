053b424968157d9a78a2e7db1df5c012d9cd0685
Christopher Goodwin <hello@christophergoodwin.me>
2026-07-31 22:44:09 -0400

    Fix font pop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.christophergoodwin.me',
+    fonts: [{
+        provider: fontProviders.local(),
+        name: "Edmondsans",
+        cssVariable: "--font-edmondsans",
+        options: {
+            variants: [{
+                src: ['./src/assets/fonts/edmondsans/edmondsans-regular.woff2', './src/assets/fonts/edmondsans/edmondsans-regular.woff'],
+                weight: '400',
+                style: 'normal'
+            },
+            {
+                src: ['./src/assets/fonts/edmondsans/edmondsans-medium.woff2', './src/assets/fonts/edmondsans/edmondsans-medium.woff'],
+                weight: '500',
+                style: 'normal'
+            },
+            {
+                src: ['./src/assets/fonts/edmondsans/edmondsans-bold.woff2', './src/assets/fonts/edmondsans/edmondsans-bold.woff'],
+                weight: '700',
+                style: 'normal'
+            }]
+        }
+    }]
 });
