6f6457bf8c66944fe0091c68c5ef97db8dde2137
Christopher Goodwin <hello@christophergoodwin.me>
2026-08-03 23:47:45 -0400

    Fixes #127 - SSR and Password Protection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,34 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.christophergoodwin.me',
-    fonts: [{
-        provider: fontProviders.local(),
-        name: "Edmondsans",
-        cssVariable: "--font-edmondsans",
-        options: {
-            variants: [{
-                src: ['./src/assets/fonts/edmondsans/edmondsans-regular.woff2'],
-                weight: '400',
-                style: 'normal'
-            },
-            {
-                src: ['./src/assets/fonts/edmondsans/edmondsans-medium.woff2'],
-                weight: '500',
-                style: 'normal'
-            },
-            {
-                src: ['./src/assets/fonts/edmondsans/edmondsans-bold.woff2'],
-                weight: '700',
-                style: 'normal'
-            }]
-        }
-    }]
+  site: 'https://www.christophergoodwin.me',
+
+  fonts: [{
+      provider: fontProviders.local(),
+      name: "Edmondsans",
+      cssVariable: "--font-edmondsans",
+      options: {
+          variants: [{
+              src: ['./src/assets/fonts/edmondsans/edmondsans-regular.woff2'],
+              weight: '400',
+              style: 'normal'
+          },
+          {
+              src: ['./src/assets/fonts/edmondsans/edmondsans-medium.woff2'],
+              weight: '500',
+              style: 'normal'
+          },
+          {
+              src: ['./src/assets/fonts/edmondsans/edmondsans-bold.woff2'],
+              weight: '700',
+              style: 'normal'
+          }]
+      }
+  }],
+    output: 'server',
+    adapter: netlify()  
 });
