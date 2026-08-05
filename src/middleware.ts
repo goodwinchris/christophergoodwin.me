d08fbfceb189bd2b33cb1124f450a0c1067cccb5
Christopher Goodwin <hello@christophergoodwin.me>
2026-08-05 11:19:33 -0400

    Fixes #131 - Log who is viewing photo feed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,28 @@
 import { defineMiddleware } from 'astro:middleware';
 
 const PROTECTED_PATHS = ['/work/private'];
+const TRACKED_PATHS = ['/photo-feed'];
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const isProtected = PROTECTED_PATHS.some(p => context.url.pathname.startsWith(p));
-  console.log('isProtected', isProtected, context.url.pathname);
-  if (!isProtected) return next();
+  const isTracked = TRACKED_PATHS.some(p => context.url.pathname.startsWith(p));
+  if (!isProtected && !isTracked) return next();
 
-  const cookie = context.cookies.get('site-auth')?.value;
+  if (isProtected) {
+    const cookie = context.cookies.get('site-auth')?.value;
+    if (cookie === import.meta.env.SITE_PASSWORD) return next();    
+    return context.redirect(`/enter-password?redirect=${context.url.pathname}`);
+  }
 
-  console.log('cookie', cookie, import.meta.env.SITE_PASSWORD);
-  if (cookie === import.meta.env.SITE_PASSWORD) return next();
-
-  return context.redirect(`/enter-password?redirect=${context.url.pathname}`);
+  if (isTracked) {
+    const cookie = context.cookies.get('logged-in-as')?.value;
+    if(cookie) {
+      fetch('https://ntfy.sh/cg_website_photo-feed_logged-in-as', {
+        method: 'POST', // PUT works too
+        body: `${cookie} is viewing the photo feed`
+      }) 
+      return next();
+    }
+    return context.redirect(`/enter-user?redirect=${context.url.pathname}`);
+  }
 });
